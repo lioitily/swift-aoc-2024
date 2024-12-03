6aa82c0e680b8fce9a32f53a3db90074ab0af7f0
lio丶 <lioitily@gmail.com>
2024-12-03 17:09:44 +0800

    chore: format codes

---
+++ Sources/Day02.swift
@@ -0,0 +1,16 @@
+import Algorithms
+
+struct Day02: AdventDay {
+    // Save your data in a corresponding text file in the `Data` directory.
+    var data: String
+
+    // Replace this with your solution for the first part of the day's challenge.
+    func part1() -> Any {
+        return 2
+    }
+
+    // Replace this with your solution for the second part of the day's challenge.
+    func part2() -> Any {
+        0
+    }
+}
--- Tests/Day01.swift
+++ Tests/Day01.swift
@@ -5,8 +5,8 @@ import Testing
 // Make a copy of this file for every day to ensure the provided smoke tests
 // pass.
 struct Day01Tests {
-  // Smoke test data provided in the challenge question
-  let testData = """
+    // Smoke test data provided in the challenge question
+    let testData = """
     3   4
     4   3
     2   5
@@ -15,13 +15,13 @@ struct Day01Tests {
     3   3
     """
 
-  @Test func testPart1() async throws {
-    let challenge = Day01(data: testData)
-    #expect(String(describing: challenge.part1()) == "11")
-  }
+    @Test func testPart1() async throws {
+        let challenge = Day01(data: testData)
+        #expect(String(describing: challenge.part1()) == "11")
+    }
 
-  @Test func testPart2() async throws {
-    let challenge = Day01(data: testData)
-    #expect(String(describing: challenge.part2()) == "31")
-  }
+    @Test func testPart2() async throws {
+        let challenge = Day01(data: testData)
+        #expect(String(describing: challenge.part2()) == "31")
+    }
 }
