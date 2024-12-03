7c22832eef3f3481f38f7e6069cc9da8ec8dfbb4
lio <lioitily@gmail.com>
2024-12-04 01:14:59 +0800

    feat: day3

---
+++ Sources/Day03.swift
@@ -0,0 +1,30 @@
+import Algorithms
+
+struct Day03: AdventDay {
+    // Save your data in a corresponding text file in the `Data` directory.
+    var data: String
+
+    func muls(_ data: String) -> [(x: Int, y: Int)] {
+        let regex = /mul\((\d+),(\d+)\)/
+        let matchs = data.matches(of: regex)
+        return matchs.compactMap { match in
+            guard let x = Int(match.1), let y = Int(match.2) else { return nil }
+            return (x, y)
+        }
+    }
+
+    func mul(_ x: Int, _ y: Int) -> Int {
+        x * y
+    }
+
+    func part1() -> Any {
+        return muls(data).map(mul).reduce(0, +)
+    }
+
+    func part2() -> Any {
+        let pattern = /don't\(\).*?(?:do\(\)|$)/
+        let data = data.replacing("\n", with: "")
+            .replacing(pattern, with: "")
+        return muls(data).map(mul).reduce(0, +)
+    }
+}
